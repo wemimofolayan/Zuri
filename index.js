11bdbaed520f3f80b171cf1981204a7889112ffd
wemimofolayan <68654903+wemimofolayan@users.noreply.github.com>
2022-06-21 18:12:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var wemimo = {
+    name: 'Oluwawemimo Folayan',
+    age: 24,
+    height: "5'11",
+    country: "Nigeria"
+};
+console.log(wemimo);
+document.getElementById("demo").innerHTML = wemimo.name + " " + " , " + wemimo.age + " " + " , " + wemimo.height + " " + " , " + wemimo.country;
+
